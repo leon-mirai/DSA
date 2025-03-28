97c6e5cc5647cf9037bfae7563809248babc7431
Leon <leon.lee4230@gmail.com>
2025-03-28 10:10:12 +1000

    stuff

--- wk3/test.py
+++ wk3/test.py
@@ -1,39 +1,33 @@
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = self.head
-        self.length = 0
-    
-    def append(self, item):
-        new_node = Node(item)
-        
-        if (self.head is None):
-            self.head = new_node
-            self.tail = new_node
-            self.length = 1
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
-        self.length += 1
-        
-
-myLL = LinkedList()
-myLL.append(5)
-print(myLL)
+def bubbleSort(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) -1):
+            if (arr[j] < arr[j + 1]):
+                arr[j], arr[j+1] = arr[j+1], arr[j]
 
 
-    # def append(self):
-        
-    # def prepend(self):
-        
-    # def insert(self):
-        
-    # def remove(self):
-        
-    # def print_linked_list(self):
-        
+def selectionSort(arr):
+    for i in range(len(arr)):
+        min = i
+        for j in range(i + 1, len(arr)):
+            if (arr[j] < arr[min]):
+                min = j
+        arr[i], arr[min] = arr[min], arr[i]
+
+def insertionSort(arr):
+    i = 1
+    end_of_sorted = arr[0]
+    length = len(arr)
+    while i < length:
+        if (arr[i] < end_of_sorted):
+            min = arr.pop(i)
+            for j in range(0, i):
+                if min < arr[j]:
+                    min = j
+
+        end_of_sorted = arr[i]
+        i += 1
+array = [5,4,2,3,1,9,17,15,13,167]
+print(array)
+insertionSort(array)
+print(array)
+
